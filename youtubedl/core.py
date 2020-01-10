b3aca70d5df4dabe231bac8c266bc6bf8c374561
Cheryl Murphy <webmaster@cherylli.com>
2020-01-10 20:16:29 +1100

    Update files to load from qt.ui directly

---
+++ youtubedl/core.py
@@ -0,0 +1,5 @@
+from pytube import YouTube
+
+yt = YouTube('https://www.youtube.com/watch?v=ksW59gYEl6Q')
+
+print(yt.title)
--- youtubedl/ui/pyside2.py
+++ youtubedl/ui/pyside2.py
@@ -1,2 +1,18 @@
 # https://doc.qt.io/qtforpython/tutorials/basictutorial/uifiles.html
+import sys
 from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import QFile
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    ui_file = QFile("qt.ui")
+    ui_file.open(QFile.ReadOnly)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+    window.show()
+
+    sys.exit(app.exec_())
