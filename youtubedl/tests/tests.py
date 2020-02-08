833fe802f060b8a48b960756c4f5bc1ebdec2121
Itwilloutliveyou <alexandre.horta@gmail.com>
2020-02-08 02:35:04 -0300

    Let's test everything.

---
+++ youtubedl/tests/tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+import core
+
+# Run tests with python -m unittest path/to/tests.py
+
+
+class TestVideo(unittest.TestCase):
+
+    def setUp(self):
+        self.video = core.Video('https://www.youtube.com/watch?v=9bZkp7q19f0')
+
+    def testVideoInit(self):
+        self.assertEqual(self.video.url, 'https://www.youtube.com/watch?v=9bZkp7q19f0')
+        self.assertEqual(self.video.progress_callback, None)
+
+    def testVideoMethods(self):
+        with self.assertRaises(NotImplementedError) as error:
+            self.video.videoId
+        self.assertEqual(error.exception, NotImplementedError)
