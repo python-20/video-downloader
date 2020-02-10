59abc5b8173c3fa8ac31760bbc9cb45a2a9c2baf
RyanSamman <56226071+RyanSamman@users.noreply.github.com>
2020-02-10 18:42:39 +0300

    Add info to setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Python-video-downloader',
+    version='0.1.1',
+    packages=[''],
+    url='',
+    license='GPL-3.0',
+    author='Notcori, Chonix, Ryan Samman',
+    author_email='',
+    description=''
+)
