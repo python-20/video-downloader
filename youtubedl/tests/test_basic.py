4437f0afa9ae07b4bd58064c7ebe055e59388b00
AlexPHorta <alexandre.horta@gmail.com>
2020-02-13 08:37:28 -0300

    First set of tests.

--- youtubedl/tests/test_basic.py
+++ youtubedl/tests/test_basic.py
@@ -5,14 +5,18 @@ from .context import core
 # Run tests with python -m unittest tests/test_basic.py
 
 
-allMethods = ['download', 'videoId', 'videoStreamQuality',
-            'videoThumbnail', 'videoTitle']
+# These are the methods used as interface with the rest of the program.
+# To include a method in Video, insert the method name here.
+videoBaseMethods = frozenset(['allVideoStreams', 'download', 'videoId',
+        'videoThumbnail', 'videoTitle'])
 
 class TestVideo(unittest.TestCase):
 
     def setUp(self):
         self.video = core.Video(
             'https://A.com')
+        self.methods = [m for m in dir(self.video) if not m.startswith('__')
+            and m not in self.video.__dict__.keys()]
 
     def testVideoInit(self):
         self.assertEqual(self.video.url,
@@ -20,19 +24,15 @@ class TestVideo(unittest.TestCase):
         self.assertEqual(self.video.progress_callback, None)
 
     def testWhichAreTheVideoMethods(self):
-        # To include a method in Video, insert the method name in allMethods
-        methods = [m for m in dir(self.video) if not m.startswith('__')
-            and m not in self.video.__dict__.keys()]
-        self.assertEqual(methods, allMethods)
+        # To include a method in Video, insert the method name in videoBaseMethods
+        for m in videoBaseMethods:
+            self.assertTrue(m in self.methods)
 
     def testVideoMethodsRaise(self):
         # All Video methods must raise NotImplementedError.
         with self.assertRaises(NotImplementedError):
-            self.video.videoId
-            self.video.videoTitle
-            self.video.videoThumbnail
-            self.video.videoStreamQuality
-            self.video.download()
+            for m in self.methods:
+                getattr(self.video, m)
 
 
 class TestYouTubeVideo(unittest.TestCase):
@@ -40,14 +40,15 @@ class TestYouTubeVideo(unittest.TestCase):
     def setUp(self):
         self.video = core.Video(
             "https://www.youtube.com/watch?v=9bZkp7q19f0")
+        self.methods = [m for m in dir(self.video) if not m.startswith('__')
+            and m not in self.video.__dict__.keys()]
 
     def testYouTubeVideoInit(self):
         self.assertEqual(self.video.url,
             "https://www.youtube.com/watch?v=9bZkp7q19f0")
         self.assertEqual(self.video.progress_callback, None)
 
-    def testWhichAreYouTubeVideoMethods(self):
+    def testYouTubeVideoBaseMethods(self):
         # To include a method in YouTubeVideo, insert the method name in Video
-        methods = [m for m in dir(self.video) if not m.startswith('__')
-            and m not in self.video.__dict__.keys()]
-        self.assertEqual(methods, allMethods)
+        for m in videoBaseMethods:
+            self.assertTrue(m in self.methods)
