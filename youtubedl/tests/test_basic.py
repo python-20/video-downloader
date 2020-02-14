a267ddd9bf06d3cd1634c8558803384a3c87e647
Cheryl Murphy <webmaster@cherylli.com>
2020-02-14 12:34:33 +1100

    Minor Linting

--- youtubedl/tests/test_basic.py
+++ youtubedl/tests/test_basic.py
@@ -8,7 +8,8 @@ from .context import core
 # These are the methods used as interface with the rest of the program.
 # To include a method in Video, insert the method name here.
 videoBaseMethods = frozenset(['allVideoStreams', 'download', 'videoId',
-        'videoThumbnail', 'videoTitle'])
+                              'videoThumbnail', 'videoTitle'])
+
 
 class TestVideo(unittest.TestCase):
 
@@ -16,15 +17,16 @@ class TestVideo(unittest.TestCase):
         self.video = core.Video(
             'https://A.com')
         self.methods = [m for m in dir(self.video) if not m.startswith('__')
-            and m not in self.video.__dict__.keys()]
+                        and m not in self.video.__dict__.keys()]
 
     def testVideoInit(self):
         self.assertEqual(self.video.url,
-            'https://A.com')
+                         'https://A.com')
         self.assertEqual(self.video.progress_callback, None)
 
     def testWhichAreTheVideoMethods(self):
-        # To include a method in Video, insert the method name in videoBaseMethods
+        # To include a method in Video, insert the method name in
+        # videoBaseMethods
         for m in videoBaseMethods:
             self.assertTrue(m in self.methods)
 
@@ -41,11 +43,11 @@ class TestYouTubeVideo(unittest.TestCase):
         self.video = core.Video(
             "https://www.youtube.com/watch?v=9bZkp7q19f0")
         self.methods = [m for m in dir(self.video) if not m.startswith('__')
-            and m not in self.video.__dict__.keys()]
+                        and m not in self.video.__dict__.keys()]
 
     def testYouTubeVideoInit(self):
         self.assertEqual(self.video.url,
-            "https://www.youtube.com/watch?v=9bZkp7q19f0")
+                         "https://www.youtube.com/watch?v=9bZkp7q19f0")
         self.assertEqual(self.video.progress_callback, None)
 
     def testYouTubeVideoBaseMethods(self):
